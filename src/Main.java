e55111c05505966240b02dcfedf48adfac522728
iamShivamSingh1 <158357732+iamShivamSingh1@users.noreply.github.com>
2024-02-01 03:51:43 +0545

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double price = 1000;
+        double SALESTX = 0.05;
+        double compoundedtax = price * SALESTX;
+        System.out.println(compoundedtax);
+
+    }
+}
